9967dedcd136c3a38a583bcdd82bae268ba47afb
Ashley Lobo <ashleyl@rediffmail.com>
2019-06-23 21:39:34 +0530

    Updates

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import pprint
+
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
 creds = ServiceAccountCredentials.from_json_keyfile_name('Nakshatra-f89da92381db.json', scope)
@@ -11,10 +11,13 @@ telemedicine = sheet.get_all_records()
 
 
 
-pp = pprint.PrettyPrinter()
 def csvToJson():
-    l=[]
-    for i in range(0,55):
-        l.append(telemedicine[i])
-    return l
-print(str(csvToJson()))
+    #mapping=telemedicine[0]
+    data={}
+    for i in range(1,len(telemedicine)):
+        if(telemedicine[i]['Value_0'] in data.keys()):
+            data[telemedicine[i]['Value_0']].append(telemedicine[i])
+        else:
+            data[telemedicine[i]['Value_0']]=[telemedicine[i]]
+    return data
+print(csvToJson())
