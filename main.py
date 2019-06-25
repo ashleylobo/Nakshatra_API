40bc2124c935e88089e6befae80d37a7a0f4574b
Ashley Lobo <ashleyl@rediffmail.com>
2019-06-25 19:15:37 +0530

    updated

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/aut
 creds = ServiceAccountCredentials.from_json_keyfile_name('Nakshatra-f89da92381db.json', scope)
 
 client = gspread.authorize(creds)
-sheet = client.open('Nakshtra 0.4').sheet1
-telemedicine = sheet.get_all_records()
+
 
 
 
 def csvToJson():
     #mapping=telemedicine[0]
+    sheet = client.open('Nakshtra 0.4').sheet1
+    telemedicine = sheet.get_all_records()
     data={}
     for i in range(1,len(telemedicine)):
         if(telemedicine[i]['Value_0'] in data.keys()):
@@ -20,4 +21,3 @@ def csvToJson():
         else:
             data[telemedicine[i]['Value_0']]=[telemedicine[i]]
     return data
-print(csvToJson())
