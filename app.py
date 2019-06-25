1974a109c3cc2ecf866fc2ffdec903de55e6b548
Ashley Lobo <ashleyl@rediffmail.com>
2019-06-25 20:59:58 +0530

    Testing

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from flask_cors import CORS, cross_origin
 import requests, json
 import pandas as pd
 import requests
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 
 
